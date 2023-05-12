4f855666a461e74c41d87936b4eda91dc8edb1f9
James Hodgkinson <james@terminaloutcomes.com>
2023-05-12 11:23:57 +1000

    checking for valid tlp/report types

--- src/README.md
+++ src/README.md
@@ -1,5 +1,7 @@
 # sambot-parser
 
+A thing for parsing a given piece of text and outputting it in [sam-bot](https://github.com/IRATEAU/sam-bot/blob/master/README.md) format.
+
 Installation: `cargo install --git https://github.com/yaleman/sambot-parser`
 
 For help, run `sambot-parser --help`
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Context;
 use clap::Parser;
+use lazy_static::lazy_static;
 use sambot_parser::process_str;
 use std::io::Read;
 
@@ -17,11 +18,38 @@ struct Cli {
     report_type: Option<String>,
 }
 
+lazy_static! {
+    static ref VALID_REPORT_TYPES: Vec<&'static str> =
+        vec!["phish", "malware", "bec/scam", "dump", "apt",];
+    static ref VALID_TLP: Vec<&'static str> = vec!["white", "green", "amber", "red",];
+}
+
 fn main() {
     // take input from stdin
     let cli = Cli::parse();
-    let tlp = cli.tlp.unwrap_or("green".to_string());
-    let report_type = cli.report_type.unwrap_or("phish".to_string());
+    let tlp = cli.tlp.unwrap_or("green".to_string()).to_lowercase();
+    let report_type = cli
+        .report_type
+        .unwrap_or("phish".to_string())
+        .to_lowercase();
+
+    if !VALID_TLP.contains(&tlp.as_str()) {
+        println!(
+            "TLP value needs to be one of: {}, not '{}'",
+            VALID_TLP.join(","),
+            tlp
+        );
+        std::process::exit(1);
+    }
+
+    if !VALID_REPORT_TYPES.contains(&report_type.as_str()) {
+        println!(
+            "Report type needs to be one of: {}, not '{}'",
+            VALID_REPORT_TYPES.join(","),
+            report_type
+        );
+        std::process::exit(1);
+    }
 
     let mut data = String::new();
 
